9467684f9ed85c248161f141aaabbbbf87ca80f4
KadhirAnand24 <kadhiranand24@gmail.com>
2026-02-25 16:50:13 +0530

    UC13 Compare the performance of different palindrome approaches:

--- src/PalindromeCheckerApp_146.java
+++ src/PalindromeCheckerApp_146.java
@@ -6,33 +6,30 @@ public class PalindromeCheckerApp_146 {
 
         String input = "level";
 
-        PalindromeStrategy strategy = new StackStrategy();
+        long start = System.nanoTime();
 
-        boolean isPalindrome = strategy.check(input);
+        boolean isPalindrome = checkPalindrome(input);
+
+        long end = System.nanoTime();
+
+        long executionTime = end - start;
 
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Execution Time : " + executionTime + " ns");
     }
-}
 
-interface PalindromeStrategy {
-    boolean check(String input);
-}
+    private static boolean checkPalindrome(String input) {
 
-class StackStrategy implements PalindromeStrategy {
+        int left = 0;
+        int right = input.length() - 1;
 
-    public boolean check(String input) {
-
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-        }
-
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
+        while (left < right) {
+            if (input.charAt(left) != input.charAt(right)) {
                 return false;
             }
+            left++;
+            right--;
         }
 
         return true;
